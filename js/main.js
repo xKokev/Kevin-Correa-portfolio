4b1681ff3a22b2709a05f94b989629c26cdf22fa
Kevin <10kevinho@gmail.com>
2021-07-10 13:43:11 -0400

    feat: :sparkles: Boton very nice feature

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const $menuNav = document.getElementById('menuPalanca'),
+  $hamburger = document.querySelector('.hamburger'),
+  palancaMenu = (_) => $menuNav.classList.toggle('palanca'),
+  hamburgerAnimated = (_) => $hamburger.classList.toggle('is-active');
+
+let continuar, continuar_play;
+
+document.addEventListener('click', (e) => {
+  if (
+    e.target.matches('.botonPalanca') ||
+    e.target.matches('.hamburger') ||
+    e.target.matches('.hamburger-inner') ||
+    e.target.matches('.hamburger-box')
+  ) {
+    if (!document.querySelector('.inRight')) {
+      palancaMenu(e);
+      hamburgerAnimated(e);
+    }
+  }
+});
+
 var animItem = bodymovin.loadAnimation({
   container: document.getElementById('anim'),
   /* renderer: 'svg', */
